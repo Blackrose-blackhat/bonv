9bd08804388070f7bfe4a469ae13afd7ba4dc583
fsociety <fsociety@localhost.localdomain>
2023-12-17 22:47:16 +0530

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import gif from "../app/assets/parallax.gif";
+import tutorial from "../app/assets/tutorial.gif";
+import { Button } from "@/components/ui/button";
+import team from "@/app/assets/team.jpeg";
+import bonv from "@/app/assets/bonv.jpeg";
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div
+        style={{
+          background: `url(${bonv.src})`,
+        }}
+        className="flex flex-col items-center justify-center w-full h-screen relative"
+      >
+        <div className="bg-black opacity-75 w-full h-screen flex flex-col items-center justify-center text-white">
+          <div className="flex flex-row">
+            <h1 className="text-shadow font-bold text-3xl mb-4 ">
+              Support Bonv
+            </h1>
+          </div>
+          <p className="text-6xl w-2/3 mt-10 font-light  text-center py-5">
+            Help us win this competition for India by casting your vote for
+            Bonv.
+          </p>
+          <p className="mt-10 w-1/2 text-center text-xl ">
+            <span className="text-blue-500">BONV</span> is on a mission to
+            democratize aerial mobility through our cutting-edge development of
+            state-of-the-art electric aerial vehicles & its ecosystem for goods
+            and people mobility
+          </p>
+          <p className="mt-5 text-center w-2/3 text-2xl ">
+            Bonv pitched their Idea on Meet the Drappers, season 6 at
+            California, competing with worldwide startup Ideas. Now its our time
+            to support them in their journey.
+          </p>
+          <div className="py-10">
+            <Link href="https://www.meetthedrapers.com/vote" target="_blank">
+              <Button className="p-6 text-md bg-blue-700 ">
+                Go Vote for Bonv
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div id="video" className="py-5">
+        <h2 className="p-5 underline text-3xl font-semibold text-neutral-700">
+          Tutorial on How to Vote:
+        </h2>
+        <Image src={tutorial} alt="tutorial" />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="w-full p-20">
+        <div
+          className="flex flex-col items-center justify-center w-full h-screen bg-cover bg-no-repeat rounded-md"
+          style={{ background: `url(${gif.src})`, backgroundSize: "cover" }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="bg-black opacity-90 w-full text-white h-screen flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold mt-96 ">Vote For BONV</h1>
+            <p className="text-xl font-thin mt-10 text-center w-1/2">
+              Also send the link{" "}
+              <a
+                className="text-blue-500 font-normal"
+                href="https://www.meetthedrapers.com/vote"
+              >
+                https://www.meetthedrapers.com/vote{" "}
+              </a>{" "}
+              to your friends and family and ask them to vote for BONV.
+              <br />
+              <span className="text-2xl font-semibold ">Thank You</span>
+            </p>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div>
+          <div className="flex flex-row w-full p-5 py-20">
+            <div className="flex-1 ">
+              <Image src={team} alt="team" quality={100} width={950} />
+            </div>
+            <div className="flex-1 flex flex-col gap-40 items-center justify-center py-20">
+              <p className="text-center text-xl  font-normal text-neutral-800">
+                At{" "}
+                <span className="text-blue-500 font-bold">
+                  <a href="https://bonvaero.com/">BONV</a>
+                </span>{" "}
+                our mission is to revolutionize the way people experience
+                convenience and safety through cutting-edge drone technology.
+                Behind every successful venture lies a dedicated team of
+                professionals committed to pushing the boundaries of innovation.
+                Meet the passionate individuals who are driving our vision to
+                provide unparalleled drone services for our valued customers
+              </p>
+              <Link href="https://bonvaero.com/" target="_blank">
+                <Button className="p-6 text-md bg-blue-700 ">
+                  {" "}
+                  Explore the world of aerial mobility
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="w-full flex flex-row justify-center items-center p-10">
+          <iframe
+            width="960"
+            height="515"
+            src="https://www.youtube.com/embed/Y04pRPRQFDU?si=apQMTVYTlrhZ0NL8"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
       </div>
     </main>
-  )
+  );
 }
